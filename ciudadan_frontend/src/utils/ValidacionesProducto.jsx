1508ec4a36c193250e4b6ae4886cda23d75442b5
Daniel Sepulveda Chiguils <chiguilsdanielsepulveda@gmail.com>
2026-07-14 09:33:20 -0600

    Add automatic cleaning of the product registration form

--- ciudadan_frontend/src/utils/ValidacionesProducto.jsx
+++ ciudadan_frontend/src/utils/ValidacionesProducto.jsx
@@ -1,5 +1,5 @@
 // src/helpers/validacionesProducto.js
-export const textoValido = /^[\w\s.,:¡!¿?()\-'"%/áéíóúüÁÉÍÓÚÜñÑ]+$/;
+export const textoValido = /^[\w\s.,:¡!¿?()\-°*+×'"%/áéíóúüÁÉÍÓÚÜñÑ]+$/;
 
 export const validarPaso1 = (formData) => {
   const nombre = formData.nombre?.trim() ?? '';
@@ -14,6 +14,18 @@ export const validarPaso1 = (formData) => {
     !categoria || isNaN(precio) || precio <= 0 ||
     (formData.stockEnabled && (isNaN(stock) || stock < 0))
   ) {
+    if(!textoValido.test(nombre)){
+      console.log("-".repeat(20));
+      console.log("Caracter raro en nombre:");
+      getInvalidChars(nombre);
+      console.log("-".repeat(20));
+    }
+    if(!textoValido.test(descripcion)){
+      console.log("-".repeat(20));
+      console.log("Caracter raro en nombre:");
+      getInvalidChars(descripcion);
+      console.log("-".repeat(20));
+    }
     return false;
   }
   return true;
@@ -22,3 +34,12 @@ export const validarPaso1 = (formData) => {
 export const validarPaso2 = (formData) => {
   return !!(formData.largo && formData.ancho && formData.alto && formData.peso);
 };
+
+export const getInvalidChars = (text='')=>{
+  [...text].forEach((c,i)=>{
+    if(!textoValido.test(c))
+      console.log(
+        `Posición ${i}: "${c}" Código: U+${c.charCodeAt(0).toString(16).toUpperCase()}`
+      );
+  });
+}
