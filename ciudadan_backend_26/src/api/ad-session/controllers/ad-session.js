bcdf7987c474ac24a7ad38afcbc67a4b9517800a
Ciudadan <ciudadanmx@gmail.com>
2026-09-01 05:37:49 -0600

    feat(ad-rewards): schemas back-end para visualización remunerada de anuncios
    - Extiende api::ad con: esPublicitario, duracion, recompensa, decisionWindow, thumbnail
    
    - Nueva coleccion ad-session (sesion de reproduccion, token, estado, metadata
    
    - Nueva coleccion ad-session-item (cobertura antifraude, tiempo efectivo, estados de item
    
    - Estructura core (controller, router, service) de ambas colecciones nuevas

---
+++ ciudadan_backend_26/src/api/ad-session/controllers/ad-session.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ad-session controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::ad-session.ad-session');
---
+++ ciudadan_backend_26/src/api/ad-session/routes/ad-session.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ad-session router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ad-session.ad-session');
