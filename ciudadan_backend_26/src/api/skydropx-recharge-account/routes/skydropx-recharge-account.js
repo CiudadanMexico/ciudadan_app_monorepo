ed17864003f80ba84e588f00365c4582a2ea7f41
Daniel Sepulveda Chiguils <chiguilsdanielsepulveda@gmail.com>
2026-09-21 15:32:55 -0600

    Añadir un tipo único «skydropx-recharge-account»Añadir un tipo único «skydropx-recharge-account»

---
+++ ciudadan_backend_26/src/api/skydropx-recharge-account/routes/skydropx-recharge-account.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * skydropx-recharge-account router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::skydropx-recharge-account.skydropx-recharge-account');
---
+++ ciudadan_backend_26/src/api/skydropx-recharge-account/services/skydropx-recharge-account.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * skydropx-recharge-account service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::skydropx-recharge-account.skydropx-recharge-account');
